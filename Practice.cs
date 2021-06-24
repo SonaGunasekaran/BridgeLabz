00791e5b7f7cc909118446e6fc5137b723e2fb90
[Sona] <[sona16061999@gmail.com]>
2021-06-24 12:58:24 +0530

    First

---
+++ Practice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitPractice
+{
+    class Practice
+    {
+        static void Main(string[] args)
+        {
+            int myNum = 12;
+            int myNum1 = 2;
+            int myNum2 = 1;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(myNum + myNum1 + myNum2);
+        }
+    }
+}
